6da4f4bbd14e75f4ec8f7e61b8ba5bfd40e25705
Bartek Banda <bbanda@squiz.net>
2019-10-07 17:02:16 +1000

    Bring files up to date with whats in Matrix instance

--- src/javascripts/bct-loader.js
+++ src/javascripts/bct-loader.js
@@ -1,65 +1,47 @@
-function iniBctWrappers(){
-    //find all bct wrappers on the screen and initialise them with the bct plugin
-    $('.bct-wrapper').not('.initialised').each(function(){
+
+// function for initialising any bct's found on the current screen
+function iniBctWrappers() {
+    // find all bct wrappers on the screen and initialise them with the bct plugin
+    $('.bct-wrapper').not('.initialised').each(function () {
         iniBct(this);
     });
-    //add class to the body to indicate that we have initialised the bct plugin
+    // add class to the body to indicate that we have initialised the bct plugin
     $('body').addClass('bct');
 }
 
-//load the required CSS and JS into the head
-if(document.getElementById('bct-js') == undefined){
-    
-    //add the JS to the head
-    var s = document.createElement('script');
+// load the required CSS and JS into the head if we haven't done it yet
+if (document.getElementById('bct-js') == undefined) {
+    // load the CSS files in the <head>
+    document.head.insertAdjacentHTML('beforeend', '\
+        <link rel="stylesheet" href="https://matrix.squiz.net/__data/assets/css_file/0022/25285/matrix-bootstrap.min.css">\
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" id="fa_stylesheet">\
+        <link rel="stylesheet" href="https://matrix.squiz.net/__data/assets/css_file/0019/26065/fontawesome-iconpicker.css">\
+        <link rel="stylesheet" href="https://matrix.squiz.net/__data/assets/css_file/0015/25341/bct.css?v=${Math.random()}+">\
+    ');
+
+    // load the JS files in the <head>
+    const s = document.createElement('script');
+    s.src = `https://matrix.squiz.net/__data/assets/js_file/0016/25315/matrix-bootstrap.min.js?v=${Math.random()}`;
     s.id = 'bct-js';
-    s.src = 'https://matrix.squiz.net/__data/assets/js_file/0016/25315/matrix-bootstrap.min.js'; // bootstrap.js
     document.head.appendChild(s);
-    
-    var s2 = document.createElement('script');
-    s2.src = 'https://matrix.squiz.net/__data/assets/js_file/0020/26066/fontawesome-iconpicker.js?v='+Math.random(); // fontawesome-iconpicker.js
+
+    const s2 = document.createElement('script');
+    s2.src = 'https://matrix.squiz.net/__data/assets/js_file/0020/26066/fontawesome-iconpicker.js';
     document.head.appendChild(s2);
-    
-    var s3 = document.createElement('script');
-    s3.onload = function(){
-        setTimeout(function(){
-            iniBctWrappers();
-        }, 50);
-    }
-    s3.src = 'https://matrix.squiz.net/__data/assets/js_file/0016/25360/bct.js?v='+Math.random(); // sq-ct.js
-    document.head.appendChild(s3);
-    
-    //add the CSS to the head
-    var c = document.createElement('link');
-    c.rel = 'stylesheet';
-    c.href = 'https://matrix.squiz.net/__data/assets/css_file/0022/25285/matrix-bootstrap.min.css'; // bootstrap.css
-    document.head.appendChild(c);
-    
-    var c2 = document.createElement('link');
-    c2.rel = 'stylesheet';
-    c2.id = 'fa_stylesheet';
-    c2.href = 'https://use.fontawesome.com/releases/v5.5.0/css/all.css'; // fontawesome.css
-    document.head.appendChild(c2);
-    
-    var c3 = document.createElement('link');
-    c3.rel = 'stylesheet';
-    c3.href = 'https://matrix.squiz.net/__data/assets/css_file/0019/26065/fontawesome-iconpicker.css?v='+Math.random(); // fontawesome-iconpicker.css
-    document.head.appendChild(c3);
-    
-    var c4 = document.createElement('link');
-    c4.rel = 'stylesheet';
-    c4.href = 'https://matrix.squiz.net/__data/assets/css_file/0015/25341/bct.css?v='+Math.random(); // sq-ct.css
-    document.head.appendChild(c4);
-    
-}else{
-    
-    //CSS and JS is already loaded, we just need to initialise the containers
-    setTimeout(function(){
-        if($('body').hasClass('bct')){
-            iniBctWrappers();
-        }
+
+    const s3 = document.createElement('script');
+    s3.onload = function () {
+    setTimeout(() => {
+        iniBctWrappers();
     }, 50);
-    
+    };
+    s3.src = `https://matrix.squiz.net/__data/assets/js_file/0016/25360/bct.js?v=${Math.random()}`; // sq-ct.js
+    document.head.appendChild(s3);
+} else {
+    // CSS and JS is already loaded, so we just need to initialise the containers
+    setTimeout(() => {
+    if ($('body').hasClass('bct')) {
+        iniBctWrappers();
+    }
+    }, 50);
 }
-
-
--- src/javascripts/bct.js
+++ src/javascripts/bct.js
@@ -1,220 +1,230 @@
 //function for initialising a content template instance
 function iniBct(theWrapper){
-    var $bctWrapper = $(theWrapper);
-    var assetId = $bctWrapper.data('asset-id');
-    var $bctMetadata = $($bctWrapper.find('.bct-metadata'));
-    $bctMetadata.show();
-    
-    //add metadata toggle button
-    $bctMetadata.before('<span class="bct-metadata-toggle" id="metadata-toggle-'+assetId+'" onclick="sqToggleMetadataPanel('+assetId+')" title="Edit component"></span>');
-    //add metadata close button
-    $bctMetadata.prepend('<span class="bct-metadata-close" onclick="sqCloseMetadataPanel('+assetId+')">X</span>');
-    
-    //add on change events to metadata fields to update component previews
-    $bctMetadata.find('.sq-form-field').on('change keyup', function(e){
-        sqUpdateComponent(this);
-    });
+  var $bctWrapper = $(theWrapper);
+  var assetId = $bctWrapper.data('asset-id');
+  var $bctMetadata = $($bctWrapper.find('.bct-metadata'));
+  $bctMetadata.show();
 
-    //add on change events using mutation observer to related asset metadata fields in Edit+
-    $bctMetadata.find('.ees_assetPicker').each(function(){
-        var targetNode = this;
-        // Options for the observer (which mutations to observe)
-        var config = { attributes: true, childList: true, subtree: true };
-        // Callback function to execute when mutations are observed
-        var callback = function(mutationsList, observer) {
-            for(let mutation of mutationsList) {
-                var relatedFieldWrapper = $(mutation.target).closest('.typeRelatedAsset');
-                sqUpdateComponentRelatedAsset(relatedFieldWrapper);
-            }
-        };
-        // Create an observer instance linked to the callback function
-        var observer = new MutationObserver(callback);
-        // Start observing the target node for configured mutations
-        observer.observe(targetNode, config);
-    });
-    
-    //click events for metadata section heading accordions
-    /*$bctMetadata.find('h4').click(function(e){
-        $(this).next().toggleClass('open');
-    });*/
-    
-    //initialise icon pickers on metadata text fields
-    $bctWrapper.find('i[data-type="icon"]').each(function(){
-        var metadataId = $(this).data('mid');
-        $('#container_'+ assetId +'_metadata_field_text_'+ metadataId +'_value').iconpicker({
-            animation: false,
-            selectedCustomClass: 'bg-primary text-light',
-            templates: {
-                popover: '<div class="mbs" style="display: none;"><div class="iconpicker-popover popover shadow" x-placement="bottom bs-popover-bottom"><div class="arrow"></div>' +
-                    '<div class="popover-title"></div><div class="popover-content"></div></div></div>',
-            }
-        }).on('iconpickerSelected', function(event){
-            sqUpdateComponent(this);
-        });
-    });
-    
-    //ini bct tabs
-    iniBctTabs(theWrapper);
-                
-    //open panels that are supposed to be open on screen load
-    if($('body').hasClass('bct-metadata-open')){
-        sqOpenMetadataPanel($('body').attr('data-open-metadata'));
-        //open any metadata tabs that are supposed to be open
-        if($('body').data('open-metadata-tab')){
-            $('#'+$('body').data('open-metadata-tab')).tab('show');
-        }
-    }   
-    
-    //add class to this container to indicate that it has been initialised
-    $bctWrapper.addClass('initialised');
+  //add metadata toggle button
+  $bctMetadata.before('<span class="bct-metadata-toggle" id="metadata-toggle-'+assetId+'" onclick="sqToggleMetadataPanel('+assetId+')" title="Edit component"></span>');
+  //add metadata close button
+  $bctMetadata.prepend('<span class="bct-metadata-close" onclick="sqCloseMetadataPanel('+assetId+')">X</span>');
+
+  //add on change events to metadata fields to update component previews
+  $bctMetadata.find('.sq-form-field').on('change keyup', function(e){
+      sqUpdateComponent(this);
+  });
+
+  //add on change events using mutation observer to related asset metadata fields in Edit+
+  $bctMetadata.find('.ees_assetPicker').each(function(){
+      var targetNode = this;
+      // Options for the observer (which mutations to observe)
+      var config = { attributes: true, childList: true, subtree: true };
+      // Callback function to execute when mutations are observed
+      var callback = function(mutationsList, observer) {
+          for(let mutation of mutationsList) {
+              var relatedFieldWrapper = $(mutation.target).closest('.typeRelatedAsset');
+          }
+          sqUpdateComponentRelatedAsset(relatedFieldWrapper);
+      };
+      // Create an observer instance linked to the callback function
+      var observer = new MutationObserver(callback);
+      // Start observing the target node for configured mutations
+      observer.observe(targetNode, config);
+  });
+
+  //initialise icon pickers on metadata text fields
+  $bctWrapper.find('i[data-type="icon"]').each(function(){
+      var metadataId = $(this).data('mid');
+      $('#container_'+ assetId +'_metadata_field_text_'+ metadataId +'_value').iconpicker({
+          animation: false,
+          selectedCustomClass: 'bg-primary text-light',
+          templates: {
+              popover: '<div class="mbs" style="display: none;"><div class="iconpicker-popover popover shadow" x-placement="bottom bs-popover-bottom"><div class="arrow"></div>' +
+                  '<div class="popover-title"></div><div class="popover-content"></div></div></div>',
+          }
+      }).on('iconpickerSelected', function(event){
+          sqUpdateComponent(this);
+      });
+  });
+
+  //initialise wysiwyg change events
+  $bctMetadata.find('.htmlarea-div').each(function(){
+      var $wysiwyg = $(this);
+      $wysiwyg.on('keydown change blur focus', function() {
+          console.log($wysiwyg.attr('id'));
+          sqUpdateComponent(this);
+      });
+  });
+
+  //ini bct tabs
+  iniBctTabs(theWrapper);
+
+  //open panels that are supposed to be open on screen load
+  if($('body').hasClass('bct-metadata-open')){
+      sqOpenMetadataPanel($('body').attr('data-open-metadata'));
+      //open any metadata tabs that are supposed to be open
+      if($('body').data('open-metadata-tab')){
+          $('#'+$('body').data('open-metadata-tab')).tab('show');
+      }
+  }
+
+  //add class to this container to indicate that it has been initialised
+  $bctWrapper.addClass('initialised');
 }
 
 
 //function for initialising metadata tabs if there are any
 function iniBctTabs(theWrapper){
-    var $bctWrapper = $(theWrapper);
-    var assetId = $bctWrapper.data('asset-id');
-    var $tabs = $bctWrapper.find('.bct-tabs');
-    if($tabs){
-        //add the markup for the nav
-        $tabs.prepend(`
-            <div class="mbs">
-                <ul class="nav nav-tabs m-0 p-2 pb-0 border-0">
-                </ul>
-            </div>
-        `);
-        //assign the newly created nav to a variable
-        var $nav = $tabs.find('.nav-tabs');
-        //for each tab pane, add a nav item
-        $tabs.find('.bct-tab-pane').each(function(i){
-            var name = $(this).data('name');
-            $nav.append(`
-                <li class="nav-item">
-                    <a class="nav-link ${($(this).hasClass('active') ? 'active' : '')}" data-toggle="tab" id="tab${i}-${assetId}" href="#pane${i}-${assetId}">${name}</a>
-                 </li>
-            `);
-            $(this).attr('id',`pane${i}-${assetId}`);
-        });
-        
-        //add additional events when tab is activated
-        $nav.find('a[data-toggle="tab"]').on('shown.bs.tab', function(e){
-            //set current active tab as data attribute on body tag
-            setActiveMetadataTab(e.target.id)
-        });
-    }
+  var $bctWrapper = $(theWrapper);
+  var assetId = $bctWrapper.data('asset-id');
+  var $tabs = $bctWrapper.find('.bct-tabs');
+  if($tabs){
+      //add the markup for the nav
+      $tabs.prepend(`
+          <div class="mbs">
+              <ul class="nav nav-tabs m-0 p-2 pb-0 border-0">
+              </ul>
+          </div>
+      `);
+      //assign the newly created nav to a variable
+      var $nav = $tabs.find('.nav-tabs');
+      //for each tab pane, add a nav item
+      $tabs.find('.bct-tab-pane').each(function(i){
+          var name = $(this).data('name');
+          $nav.append(`
+              <li class="nav-item">
+                  <a class="nav-link ${($(this).hasClass('active') ? 'active' : '')}" data-toggle="tab" id="tab${i}-${assetId}" href="#pane${i}-${assetId}">${name}</a>
+               </li>
+          `);
+          $(this).attr('id',`pane${i}-${assetId}`);
+      });
+
+      //add additional events when tab is activated
+      $nav.find('a[data-toggle="tab"]').on('shown.bs.tab', function(e){
+          //set current active tab as data attribute on body tag
+          setActiveMetadataTab(e.target.id)
+      });
+  }
 }
 
 //function for setting the active metadata tab state in the body tag
 function setActiveMetadataTab(tabId){
-    var tabId = tabId;
-    $('body').attr('data-open-metadata-tab', tabId);
+  var tabId = tabId;
+  $('body').attr('data-open-metadata-tab', tabId);
 }
 
 //function for toggling the metadata panel open and close
 function sqToggleMetadataPanel(assetId){
-    //if there is already one open, either close the current one or open a new one
-    if($('body').hasClass('bct-metadata-open')){
-        //this means there's already one open
-        if($('body').attr('data-open-metadata') == assetId){
-            //if the clicked asset ID is open, just close it
-            sqCloseMetadataPanel(assetId);
-        }else{
-            //else, close any opened ones and open a new one
-            sqCloseMetadataPanel($('body').attr('data-open-metadata'));
-            sqOpenMetadataPanel(assetId);
-        }
-    }else{
-        //else just open a new one
-        sqOpenMetadataPanel(assetId);
-    }
+  //if there is already one open, either close the current one or open a new one
+  if($('body').hasClass('bct-metadata-open')){
+      //this means there's already one open
+      if($('body').attr('data-open-metadata') == assetId){
+          //if the clicked asset ID is open, just close it
+          sqCloseMetadataPanel(assetId);
+      }else{
+          //else, close any opened ones and open a new one
+          sqCloseMetadataPanel($('body').attr('data-open-metadata'));
+          sqOpenMetadataPanel(assetId);
+      }
+  }else{
+      //else just open a new one
+      sqOpenMetadataPanel(assetId);
+  }
 }
 
 //function for opening the metadata sidepanel
 function sqOpenMetadataPanel(assetId){
-    //remove open class from any other metadata panels
-    $('.bct-wrapper').not('#bct-wrapper-'+assetId).find('.bct-metadata').removeClass('open');
-    //add the open class to the clicked metadata panel (this opens it)
-    $('#bct-wrapper-'+assetId).find('.bct-metadata').addClass('open');
-    $('#metadata-toggle-'+assetId).addClass('active');
-    //set active metadata panel state in the body tag
-    $('body').addClass('bct-metadata-open');
-    $('body').attr('data-open-metadata', assetId);
-    //set the active metadata panel tag (if there is one)
-    $('.bct-metadata.open').find('.bct-tabs .nav-link.active').each(function(){
-        setActiveMetadataTab(this.id);
-    });
+  //remove open class from any other metadata panels
+  $('.bct-wrapper').not('#bct-wrapper-'+assetId).find('.bct-metadata').removeClass('open');
+  //add the open class to the clicked metadata panel (this opens it)
+  $('#bct-wrapper-'+assetId).find('.bct-metadata').addClass('open');
+  $('#metadata-toggle-'+assetId).addClass('active');
+  //set active metadata panel state in the body tag
+  $('body').addClass('bct-metadata-open');
+  $('body').attr('data-open-metadata', assetId);
 }
 
 //function for closing the metadata sidepanel
 function sqCloseMetadataPanel(assetId){
-    //remove open class from all metadata panels
-    $('.bct-metadata').removeClass('open');
-    $('#metadata-toggle-'+assetId).removeClass('active');
-    //set state in body tag to indicate that no panels are open
-    $('body').removeClass('bct-metadata-open');
-    $('body').attr('data-open-metadata', '');
+  //remove open class from all metadata panels
+  $('.bct-metadata').removeClass('open');
+  $('#metadata-toggle-'+assetId).removeClass('active');
+  //set state in body tag to indicate that no panels are open
+  $('body').removeClass('bct-metadata-open');
+  $('body').attr('data-open-metadata', '');
 }
 
 //function for updating the component preview content when metadata is changed
 function sqUpdateComponent(input){
-    var $theInput = $(input);
-    var $metadataInputWrapper = $theInput.closest('.sq-metadata-wrapper');
-    if($theInput.attr('id') != undefined){
-        var value = $theInput.val();
-        //if it's the default checkbox that was changed, we need to get the value differently
-        if($theInput.hasClass('defaultCheckbox')){
-            value = $metadataInputWrapper.find('.sq-metadata-contents-wrapper > .sq-form-field').val();
-        }
-        var ids = $theInput.attr('id').replace(/\D+/g,',').split(',');
-        var containerId = ids[1];
-        var inputId = ids[2];
-        var $container = $('#bct-wrapper-'+ containerId);
-        var $element = $container.find('[data-mid="'+ inputId +'"]');
-        var prependVal = '';
-        //add prepend value if it's available
-        if($element.data('prepend')){
-            prependVal = $element.data('prepend');
-        }
-        if($element.data('type') == 'icon'){
-            //fontawesome icon
-            $element.attr('class', value); 
-        }else if($element.data('type') == 'class'){
-            //class
-            $element.attr('class', prependVal + value); 
-        }else{
-            //normal input value
-            $element.text(value); 
-        }
-    }
+  var $theInput = $(input);
+  var $metadataInputWrapper = $theInput.closest('.sq-metadata-wrapper');
+  if($theInput.attr('id') != undefined){
+      var ids = $theInput.attr('id').replace(/\D+/g,',').split(',');
+      var containerId = ids[1];
+      var inputId = ids[2];
+      var $container = $('#bct-wrapper-'+ containerId);
+      var $element = $container.find('[data-mid="'+ inputId +'"]');
+      //get the value
+      var value = $theInput.val();
+      if($theInput.hasClass('defaultCheckbox')){
+          //if it's the default checkbox that was changed, we need to get the value differently
+          value = $metadataInputWrapper.find('.sq-metadata-contents-wrapper > .sq-form-field').val();
+      }else if($theInput.hasClass('htmlarea-div')){
+          //the input is a wysiwyg
+          value = $theInput.html();
+      }
+      var prependVal = '';
+      //add prepend value if it's available
+      if($element.data('prepend')){
+          prependVal = $element.data('prepend');
+      }
+      if($element.data('type') == 'icon'){
+          //fontawesome icon
+          $element.attr('class', value);
+      }else if($element.data('type') == 'class'){
+          //class
+          $element.attr('class', prependVal + value);
+      }else if($element.data('type') == 'class'){
+          //class
+          $element.attr('class', prependVal + value);
+      }else if($theInput.hasClass('htmlarea-div')){
+          //wysiwyg
+          $element.html(value);
+      }else{
+          //normal input value
+          $element.text(value);
+      }
+  }
 }
 
 //function for updating the component preview when related asset metadata is changed
 function sqUpdateComponentRelatedAsset(wrapperDiv){
-    var $relatedFieldWrapper = $(wrapperDiv);
-    var value = $relatedFieldWrapper.find("input[id*='value[0][assetid]']").val();
-    var ids = $relatedFieldWrapper.attr('id').replace(/\D+/g,',').split(',');
-    var containerId = ids[1];
-    var inputId = ids[2];
-    var $container = $('#bct-wrapper-'+ containerId);
-    var $element = $container.find('[data-mid="'+ inputId +'"]');
-    var prependVal = '';
-    //add prepend value if it's available
-    if($element.data('prepend')){
-        prependVal = $element.data('prepend');
-    }
-    if($element.is('img')){
-        //image tag
-        if(value > 0){
-            $element.attr('src', './?a='+value).show();
-        }else{
-            $element.attr('src', '').hide();
-        }
-    }else if($element.data('type') == 'bg-image'){
-        //background image
-        $element.css('background-image', prependVal + ' url(./?a='+ value +')');
-    }else{
-        //normal input value
-        $element.text(value);
-    }
+  var $relatedFieldWrapper = $(wrapperDiv);
+  var value = $relatedFieldWrapper.find("input[id*='value[0][assetid]']").val();
+  var ids = $relatedFieldWrapper.attr('id').replace(/\D+/g,',').split(',');
+  var containerId = ids[1];
+  var inputId = ids[2];
+  var $container = $('#bct-wrapper-'+ containerId);
+  var $element = $container.find('[data-mid="'+ inputId +'"]');
+  var prependVal = '';
+  //add prepend value if it's available
+  if($element.data('prepend')){
+      prependVal = $element.data('prepend');
+  }
+  if($element.is('img')){
+      //image tag
+      if(value > 0){
+          $element.attr('src', './?a='+value).show();
+      }else{
+          $element.attr('src', '').hide();
+      }
+  }else if($element.data('type') == 'bg-image'){
+      //background image
+      $element.css('background-image', prependVal + ' url(./?a='+ value +')');
+  }else{
+      //normal input value
+      $element.text(value);
+  }
 }
-    
+
