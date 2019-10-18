124e8954b2c210b256263bedd41719b0a4921d2f
Marcin Rek <mrek@squiz.pl>
2019-10-18 10:56:01 +1000

    Create compiling process for CSS and JS files

--- config/webpack.plugins.js
+++ config/webpack.plugins.js
@@ -9,6 +9,7 @@ const HTMLWebpackPlugin = require('html-webpack-plugin');
 const StyleLintPlugin = require('stylelint-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const ConcatPlugin = require('webpack-concat-plugin');
 
 const config = require('./site.config');
 
@@ -40,7 +41,7 @@ const stylelint = new StyleLintPlugin();
 
 // Extract CSS
 const cssExtract = new MiniCssExtractPlugin({
-  filename: 'style.[contenthash].css',
+  filename: 'bct.min.css',
 });
 
 // HTML generation
@@ -61,6 +62,36 @@ const generateHTMLPlugins = () => glob.sync('./src/*.html').map((dir) => {
   });
 });
 
+const concatBCTJS = new ConcatPlugin({
+  uglify: true,
+  sourceMap: false,
+  name: 'bct',
+  outputPath: '',
+  fileName: '[name].min.js',
+  filesToConcat: [
+    './js/matrix-bootstrap.min.js',
+    './js/fontawesome-iconpicker.js',
+    './js/bct.js'
+  ],
+  attributes: {
+    async: false
+  }
+});
+
+const concatBCTJSLoader = new ConcatPlugin({
+  uglify: false,
+  sourceMap: false,
+  name: 'bct-loader',
+  outputPath: '',
+  fileName: '[name].js',
+  filesToConcat: [
+    './js/bct-loader.js'
+  ],
+  attributes: {
+    async: false
+  }
+});
+
 // Webpack bar
 const webpackBar = new WebpackBar({
   color: '#ff6469',
@@ -71,6 +102,8 @@ module.exports = [
   stylelint,
   cssExtract,
   ...generateHTMLPlugins(),
+  concatBCTJS,
+  concatBCTJSLoader,
   config.env === 'production' && optimizeCss,
   webpackBar,
   config.env === 'development' && hmr,
