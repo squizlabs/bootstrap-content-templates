4299ae2df0000f70b0be5f8434fa20a6adbc1286
Bartek Banda <bbanda@squiz.net>
2020-02-09 13:19:41 +1000

    Fix several css issues 1. Top margins on headings and p tags in wysiwygs 2. Inherited colors in wysiwygys 3. Max and min widths on wysiwygs and images

--- src/js/bct-loader.js
+++ src/js/bct-loader.js
@@ -1,13 +1,13 @@
 /*!
-  * Bootstrap Content Templates v1.0.3
+  * Bootstrap Content Templates v1.0.5
   */
 
 /* global bctInit */
 
 //bct plugin vars
-var bctCssMin =           'https://cdn.jsdelivr.net/gh/squizlabs/bootstrap-content-templates@1.0.3/dist/bct.min.css';
+var bctCssMin =           'https://cdn.jsdelivr.net/gh/squizlabs/bootstrap-content-templates@1.0.5/dist/bct.min.css';
 var bctCssFontawesome =   'https://use.fontawesome.com/releases/v5.5.0/css/all.css';
-var bctJsMin =            'https://cdn.jsdelivr.net/gh/squizlabs/bootstrap-content-templates@1.0.3/dist/bct.min.js';
+var bctJsMin =            'https://cdn.jsdelivr.net/gh/squizlabs/bootstrap-content-templates@1.0.5/dist/bct.min.js';
 
 //function for initialising the bct plugin
 function iniBctWrappers(){
