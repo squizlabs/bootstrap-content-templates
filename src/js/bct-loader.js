d3b4c085d571b0f0d451df440de8ad6d22461aa3
Bartek Banda <bbanda@squiz.net>
2020-12-05 15:31:26 +1000

    fix alignment of icon picker and add timeout function call for initialisation function, tag 2.0.8

--- src/js/bct-loader.js
+++ src/js/bct-loader.js
@@ -1,6 +1,6 @@
 /*!
   * Bootstrap Content Templates (master)
-  * Matrix 6: 2.0.7 (tag)
+  * Matrix 6: 2.0.8 (tag)
   * Matrix 5: 1.0.8 (tag)
   */
 
@@ -8,9 +8,9 @@
 
 //bct plugin vars
 var matrixVersion = 6;
-var bctCssMin =           'https://cdn.jsdelivr.net/gh/squizlabs/bootstrap-content-templates@2.0.7/dist/bct.min.css';
+var bctCssMin =           'https://cdn.jsdelivr.net/gh/squizlabs/bootstrap-content-templates@2.0.8/dist/bct.min.css';
 var bctCssFontawesome =   'https://use.fontawesome.com/releases/v5.5.0/css/all.css';
-var bctJsMin =            'https://cdn.jsdelivr.net/gh/squizlabs/bootstrap-content-templates@2.0.7/dist/bct.min.js';
+var bctJsMin =            'https://cdn.jsdelivr.net/gh/squizlabs/bootstrap-content-templates@2.0.8/dist/bct.min.js';
 
 //matrix 5
 var m5_bctCssMin =       'https://cdn.jsdelivr.net/gh/squizlabs/bootstrap-content-templates@1.0.8/dist/bct.min.css';
@@ -42,55 +42,58 @@ function iniBctWrappers(){
     $('body').addClass('bct');
 }
 
-//load the required CSS and JS into the head if we haven't done it yet
-if(!$('body').hasClass('bct-files-loaded')){
+//put everything in a tiny deferred load to give the overall UI a chance to load everything
+setTimeout(function(){
 
-    //load the CSS files
-    document.head.insertAdjacentHTML('beforeend', '\
-        <link rel="stylesheet" href="'+ bctCssMin +'">\
-        <link rel="stylesheet" href="'+ bctCssFontawesome +'" id="fa_stylesheet">\
-    ');
+    //load the required CSS and JS into the head if we haven't done it yet
+    if(!$('body').hasClass('bct-files-loaded')){
 
-    //load the JS files
-    $.getScript(bctJsMin)
-        .done(function() {
+        //load the CSS files
+        document.head.insertAdjacentHTML('beforeend', '\
+            <link rel="stylesheet" href="'+ bctCssMin +'">\
+            <link rel="stylesheet" href="'+ bctCssFontawesome +'" id="fa_stylesheet">\
+        ');
+
+        //load the JS files
+        $.getScript(bctJsMin)
+            .done(function() {
+                setTimeout(function(){
+                    iniBctWrappers();
+                }, 100);
+            }
+        );
+
+        //add class to body to indicate that we've loaded all the files
+        $('body').addClass('bct-files-loaded');
+
+    }else{
+        //we've already loaded the files, so we just need to initialise the plugin
+
+        if(window.location.href.indexOf('_admin') > -1) {
+            //if we are in admin mode, we need to reload some of the scripts
+            if(!$('#main_form').hasClass('bct-admin-loaded')){
+                //in admin we need to reload this CSS file into the body tag because of a weird bug
+                document.body.insertAdjacentHTML('beforeend', '<link rel="stylesheet" href="'+ bctCssMin +'">');
+                //reload the JS files using a promise, when all are completed, we can call the bct ini function again
+                $.when(
+                    $.getScript(bctJsMin),
+                    $.Deferred(function( deferred ){
+                        $( deferred.resolve );
+                    }),
+                    //add class to the main admin form tag to indicate we've reloaded the scripts in admin
+                    $('#main_form').addClass('bct-admin-loaded')
+                ).done(function(){
+                    iniBctWrappers();
+                });
+            }
+        }else{
+            //if we're in /_edit, we can just call the ini function again
             setTimeout(function(){
-                iniBctWrappers();
+                if($('body').hasClass('bct')){
+                    iniBctWrappers();
+                }
             }, 100);
         }
-    );
-
-    //add class to body to indicate that we've loaded all the files
-    $('body').addClass('bct-files-loaded');
-
-}else{
-    //we've already loaded the files, so we just need to initialise the plugin
-
-    if(window.location.href.indexOf('_admin') > -1) {
-        //if we are in admin mode, we need to reload some of the scripts
-        if(!$('#main_form').hasClass('bct-admin-loaded')){
-            //in admin we need to reload this CSS file into the body tag because of a weird bug
-            document.body.insertAdjacentHTML('beforeend', '<link rel="stylesheet" href="'+ bctCssMin +'">');
-            //reload the JS files using a promise, when all are completed, we can call the bct ini function again
-            $.when(
-                $.getScript(bctJsMin),
-                $.Deferred(function( deferred ){
-                    $( deferred.resolve );
-                }),
-                //add class to the main admin form tag to indicate we've reloaded the scripts in admin
-                $('#main_form').addClass('bct-admin-loaded')
-            ).done(function(){
-                iniBctWrappers();
-            });
-        }
-    }else{
-        //if we're in Edit+, we can just call the ini function again
-        setTimeout(function(){
-            if($('body').hasClass('bct')){
-                iniBctWrappers();
-            }
-        }, 100);
     }
-}
 
-
+}, 100);
